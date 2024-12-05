3f85e5744a0213e007eb7742848347c2c8af1ddd
Katie <5213160+KatieLG@users.noreply.github.com>
2024-12-05 07:31:12 +0000

    day05 simplify python parts

--- AOC/Python/solutions/day_05.py
+++ AOC/Python/solutions/day_05.py
@@ -12,38 +12,31 @@ class Day05(AOCSolution):
         self.rules = {tuple(map(int, rule.split("|"))) for rule in rules}
         self.updates = [list(map(int, update.split(","))) for update in updates]
 
-    def is_valid_update(self, update: list[int]) -> bool:
-        """Check if an update satisfies the ordering rules"""
-        for i, page in enumerate(update):
-            for after in update[i + 1 :]:
-                if (after, page) in self.rules:
-                    return False
-        return True
-
-    def part_fix_update(self, update: list[int]) -> list[int]:
-        """Swap first two update elements that are in the wrong order"""
+    def invalid_positions(self, update: list[int]) -> tuple[int, int] | None:
+        """Find first invalid position (if any) baded on the ordering rules"""
         for i, page in enumerate(update):
             for j, after in enumerate(update[i + 1 :], start=i + 1):
                 if (after, page) in self.rules:
-                    update[i], update[j] = after, page
-                    return update
-        return update
+                    return i, j
 
     def part_one(self) -> int:
         """Sum the middle number of valid updates"""
         return sum(
             update[len(update) // 2]
-            for update in filter(self.is_valid_update, self.updates)
+            for update in self.updates
+            if not self.invalid_positions(update)
         )
 
     def part_two(self) -> int:
         """Sum the middle number of fixed invalid updates"""
         total = 0
         for update in self.updates:
-            if self.is_valid_update(update):
+            if not (coords := self.invalid_positions(update)):
                 continue
-            while not self.is_valid_update(update):
-                update = self.part_fix_update(update)
+            while coords:
+                i, j = coords
+                update[i], update[j] = update[j], update[i]
+                coords = self.invalid_positions(update)
             total += update[len(update) // 2]
         return total
 
