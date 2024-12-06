5011d7b0485cb010a954b02181fe72210e33f410
Katie Gardner <5213160+KatieLG@users.noreply.github.com>
2024-12-06 19:45:56 +0000

    day06 typescript, python b

--- AOC/Python/solutions/day_06.py
+++ AOC/Python/solutions/day_06.py
@@ -1,33 +1,58 @@
+from copy import deepcopy
+
 from models.aoc_solution import AOCSolution
 
 
 class Day06(AOCSolution):
     EXPECTED = {
         "part_one": {"sample": 41, "data": 4374},
-        "part_two": {"sample": 0, "data": 0},
+        "part_two": {"sample": 6, "data": 1705},
     }
 
     def __post_init__(self) -> None:
-        self.grid = self.data.splitlines()
+        self.grid = [[*line] for line in self.data.splitlines()]
         self.width = len(self.grid[0])
         self.height = len(self.grid)
         caret = self.data.index("^")
         self.start = caret % (self.width + 1), caret // (self.width + 1)
 
-    def part_one(self) -> int:
+    def visited_positions(self) -> tuple[set[tuple[int, int]], bool]:
+        """Returns visited positions, as well as whether there is a loop"""
         x, y = self.start
-        positions = {(x, y)}
         dx, dy = 0, -1
-        while x + dx in range(self.width) and y + dy in range(self.height):
+        positions = {(x, y)}
+        states = {(x, y, dx, dy)}
+        while 0 <= x + dx < self.width and 0 <= y + dy < self.height:
             if self.grid[y + dy][x + dx] == "#":
                 dx, dy = -dy, dx
-            x += dx
-            y += dy
-            positions.add((x, y))
+            else:
+                x += dx
+                y += dy
+                positions.add((x, y))
+            if (x, y, dx, dy) in states:
+                return positions, True
+            states.add((x, y, dx, dy))
+        return positions, False
+
+
+    def part_one(self) -> int:
+        """Turn 90deg at every # until moving off the grid"""
+        positions, _ = self.visited_positions()
         return len(positions)
 
     def part_two(self) -> int:
-        return 0
+        """Brute force"""
+        positions, _ = self.visited_positions()
+        obstacles: set[tuple[int, int]] = set()
+        for x, y in positions:
+            if self.grid[y][x] != ".":
+                continue
+            self.grid[y][x] = "#"
+            _, loop = self.visited_positions()
+            if loop:
+                obstacles.add((x, y))
+            self.grid[y][x] = "."
+        return len(obstacles)
 
 
 if __name__ == "__main__":
