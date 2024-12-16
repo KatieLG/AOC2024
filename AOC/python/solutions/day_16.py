950eaa8fe15e438cc654a03d69d975db466b353f
Katie <5213160+KatieLG@users.noreply.github.com>
2024-12-16 21:28:23 +0000

    day16 python part a

---
+++ AOC/python/solutions/day_16.py
@@ -0,0 +1,74 @@
+import sys
+from typing import NamedTuple
+
+from models.aoc_solution import AOCSolution
+
+type Coord = tuple[int, int]
+
+sys.setrecursionlimit(10000)
+
+
+class Position(NamedTuple):
+    x: int
+    y: int
+    dx: int
+    dy: int
+
+
+class Day16(AOCSolution):
+    EXPECTED = {
+        "part_one": {"sample": 11048, "data": 85420},
+        "part_two": {"sample": 0, "data": 0},
+    }
+
+    def __post_init__(self) -> None:
+        self.grid = self.data.splitlines()
+        self.start = self.find_char("S")
+        self.end = self.find_char("E")
+        self.direction = (1, 0)  # begin facing east
+
+    def find_char(self, char: str) -> tuple[int, int]:
+        return next(
+            (x, y)
+            for y, row in enumerate(self.grid)
+            for x, cell in enumerate(row)
+            if cell == char
+        )
+
+    def nbrs(self, position: Position) -> list[tuple[int, Position]]:
+        """Find the valid neigbours and their costs, pair rotations with movements to make things easier"""
+        x, y, dx, dy = position
+        output = [
+            (1, Position(x + dx, y + dy, dx, dy)),  # foward
+            (1001, Position(x - dy, y + dx, -dy, dx)),  # clockwise + foward
+            (1001, Position(x + dy, y - dx, dy, -dx)),  # counterclockwise + forward
+        ]
+        return [(cost, nbr) for cost, nbr in output if self.grid[nbr.y][nbr.x] != "#"]
+
+    def path(
+        self, position: Position, costs: list[list[int]], current_cost: int = 0
+    ) -> None:
+        """Path through the maze marking all tiles with costs"""
+        for cost, nbr in self.nbrs(position):
+            if current_cost + cost < costs[nbr.y][nbr.x]:
+                costs[nbr.y][nbr.x] = cost + current_cost
+                self.path(nbr, costs, cost + current_cost)
+
+    def generate_costs(self, start_coord: Coord, start_dir: Coord) -> list[list[int]]:
+        """Generate a grid of the costs to go from the start position to any other"""
+        costs = [[int(1e5 - 1) for _ in row] for row in self.grid]
+        start_position = Position(*start_coord, *start_dir)
+        costs[start_position.y][start_position.x] = 0
+        self.path(start_position, costs)
+        return costs
+
+    def part_one(self) -> int:
+        costs = self.generate_costs(self.start, self.direction)
+        return costs[self.end[1]][self.end[0]]
+
+    def part_two(self) -> int:
+        return 0
+
+
+if __name__ == "__main__":
+    Day16().run()
--- AOC/python/tests/test_solutions.py
+++ AOC/python/tests/test_solutions.py
@@ -10,21 +10,25 @@ today = datetime.now()
 
 
 def _load_module(name: str) -> ModuleType | None:
-    """Dont test modules that don't exist yet"""
+    """Don't test modules that don't exist yet"""
     try:
         return import_module(name)
     except ModuleNotFoundError:
         return None
 
 
-@pytest.mark.skipif(
-    today.month != 12 or today.year != 2024, reason="Only runs during December 2024"
-)
-@pytest.mark.parametrize("day", range(1, min(today.day, 25) + 1))
+MODULES = [
+    module
+    for day in range(1, 26)
+    if (module := _load_module(f"solutions.day_{day:02d}"))
+]
+
+
+@pytest.mark.parametrize("module", MODULES)
 @pytest.mark.parametrize("dataset", Dataset)
 @pytest.mark.parametrize("part", ["part_one", "part_two"])
-def test_solution(day: int, dataset: Dataset, part: str) -> None:
-    if module := _load_module(f"solutions.day_{day:02d}"):
-        solution = getattr(module, f"Day{day:02d}")()
-        solution.set_data(part, dataset)
-        assert getattr(solution, part)() == solution.EXPECTED[part][dataset.value]
+def test_solution(module: ModuleType, dataset: Dataset, part: str) -> None:
+    day = module.__name__[-2:]
+    solution = getattr(module, f"Day{day}")()
+    solution.set_data(part, dataset)
+    assert getattr(solution, part)() == solution.EXPECTED[part][dataset.value]
