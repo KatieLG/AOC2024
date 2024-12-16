e645bc2c9686cdc22bd1f7d83afb91a906557dbe
Katie <5213160+KatieLG@users.noreply.github.com>
2024-12-16 06:12:04 +0000

    day16 uiua part a

--- AOC/python/tests/test_solutions.py
+++ AOC/python/tests/test_solutions.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from importlib import import_module
+from types import ModuleType
 
 import pytest
 
 from models.aoc_solution import Dataset
 
-today = datetime.today()
+today = datetime.now()
+
+
+def _load_module(name: str) -> ModuleType | None:
+    """Dont test modules that don't exist yet"""
+    try:
+        return import_module(name)
+    except ModuleNotFoundError:
+        return None
 
 
 @pytest.mark.skipif(
@@ -15,7 +24,7 @@ today = datetime.today()
 @pytest.mark.parametrize("dataset", Dataset)
 @pytest.mark.parametrize("part", ["part_one", "part_two"])
 def test_solution(day: int, dataset: Dataset, part: str) -> None:
-    module = import_module(f"solutions.day_{day:02d}")
-    solution = getattr(module, f"Day{day:02d}")()
-    solution.set_data(part, dataset)
-    assert getattr(solution, part)() == solution.EXPECTED[part][dataset.value]
+    if module := _load_module(f"solutions.day_{day:02d}"):
+        solution = getattr(module, f"Day{day:02d}")()
+        solution.set_data(part, dataset)
+        assert getattr(solution, part)() == solution.EXPECTED[part][dataset.value]
