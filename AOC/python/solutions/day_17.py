f9a5fe5f9a00fa22c9f9fe5575b60a69a73520cd
Katie <5213160+KatieLG@users.noreply.github.com>
2024-12-17 20:21:11 +0000

    day17 python part b

--- AOC/python/solutions/day_17.py
+++ AOC/python/solutions/day_17.py
@@ -6,7 +6,7 @@ from models.aoc_solution import AOCSolution
 class Day17(AOCSolution):
     EXPECTED = {
         "part_one": {"sample": "4,6,3,5,6,3,5,2,1,0", "data": "1,5,0,1,7,4,1,0,3"},
-        "part_two": {"sample": 117440, "data": 0},
+        "part_two": {"sample": 117440, "data": 47910079998866},
     }
 
     def __post_init__(self) -> None:
@@ -17,7 +17,7 @@ class Day17(AOCSolution):
         self.registers = (a[0], b[0], c[0])
         self.program = program
 
-    def run_code(self, registers: tuple[int, int, int]) -> str:
+    def run_code(self, registers: tuple[int, int, int]) -> list[int]:
         a, b, c = registers
         pointer = 0
         output: list[int] = []
@@ -42,13 +42,57 @@ class Day17(AOCSolution):
             elif op == 7:  # cdv
                 c = a // 2**combo
             pointer += 2
-        return ",".join(map(str, output))
+        return output
 
     def part_one(self) -> str:
-        return self.run_code(self.registers)
+        return ",".join(map(str, self.run_code(self.registers)))
 
     def part_two(self) -> int:
-        return 0
+        """
+        Translating the input program into hardcoded form, it reads as follows:
+        (2, 4): b = a % 8
+        (1, 6): b = b ^ 6
+        (7, 5): c = a // 2**b
+        (4, 4): b = b ^ c
+        (1, 7): b = b ^ 7
+        (0, 3): a = a // 2**3
+        (5, 5): print(b % 8)
+        (3, 0): if a != 0: goto 0
+
+        Which simplifies to:
+        b = a % 8
+        b = b ^ 6
+        b = b ^ (a // 2**b) ^ 7
+        a = a // 8
+        print(b % 8)
+        if a != 0: goto 0
+
+        For the sample:
+        (0, 3): a = a // 8
+        (5, 4): print(a % 8)
+        (3, 0): if a != 0: goto 0
+
+        For the sample, just keep multiplying by 8 and getting the number that gives the correct output mod 8
+        a = 0
+        for r in reversed(self.program[:-1]):
+            a += r
+            a *= 8
+
+        For the program, roughly the same
+        valid = [0]
+        then for each character from the end, keep any of the 8 values that output that character first
+        """
+        print(self.program)
+        a, b, c = self.registers
+        valid = [0]
+        for r in reversed(self.program):
+            valid = [
+                8 * a + k
+                for a in valid
+                for k in range(8)
+                if self.run_code((8 * a + k, b, c))[0] == r
+            ]
+        return valid[0]
 
 
 if __name__ == "__main__":
