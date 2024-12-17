686286b851a49c8ead9b547aa271ada64c622659
Katie <5213160+KatieLG@users.noreply.github.com>
2024-12-17 18:42:12 +0000

    day17 shorter python part a

--- AOC/python/solutions/day_17.py
+++ AOC/python/solutions/day_17.py
@@ -14,89 +14,38 @@ class Day17(AOCSolution):
             list(map(int, re.findall(r"(\d+)", line)))
             for line in self.data.splitlines()
         ]
-        self.registers = [a[0], b[0], c[0]]
-        self.a, self.b, self.c = self.registers
+        self.registers = (a[0], b[0], c[0])
         self.program = program
-        self.pointer = 0
-        self.output: list[int] = []
 
-    def reset(self) -> None:
-        self.a, self.b, self.c = self.registers
-        self.pointer = 0
-        self.output = []
-
-    def division(self) -> int:
-        operand = self.get_combo_operand()
-        n, d = self.a, 2**operand
-        return n // d
-
-    def adv(self) -> None:
-        self.a = self.division()
-
-    def bxl(self) -> None:
-        operand = self.get_literal_operand()
-        self.b = self.b ^ operand
-
-    def bst(self) -> None:
-        operand = self.get_combo_operand()
-        self.b = operand % 8
-
-    def jnz(self) -> None:
-        """Don't move pointer forward 2 if jumped"""
-        if self.a != 0:
-            self.pointer = self.get_literal_operand()
-            self.pointer -= 2
-
-    def bxc(self) -> None:
-        self.b = self.b ^ self.c
-
-    def out(self) -> None:
-        self.output.append(self.get_combo_operand() % 8)
-
-    def bdv(self) -> None:
-        self.b = self.division()
-
-    def cdv(self) -> None:
-        self.c = self.division()
-
-    def get_literal_operand(self) -> int:
-        return self.program[self.pointer + 1]
-
-    def get_combo_operand(self) -> int:
-        operand = self.program[self.pointer + 1]
-        return {
-            0: 0,
-            1: 1,
-            2: 2,
-            3: 3,
-            4: self.a,
-            5: self.b,
-            6: self.c,
-        }[operand]
-
-    def handle_opcode(self, opcode: int) -> None:
-        func = {
-            0: self.adv,
-            1: self.bxl,
-            2: self.bst,
-            3: self.jnz,
-            4: self.bxc,
-            5: self.out,
-            6: self.bdv,
-            7: self.cdv,
-        }[opcode]
-        func()
-
-    def run_code(self) -> str:
-        self.reset()
-        while self.pointer < len(self.program):
-            opcode = self.program[self.pointer]
-            self.handle_opcode(opcode)
-            self.pointer += 2
-        return ",".join(map(str, self.output))
+    def run_code(self, registers: tuple[int, int, int]) -> str:
+        a, b, c = registers
+        pointer = 0
+        output: list[int] = []
+        while pointer < len(self.program):
+            op = self.program[pointer]
+            operand = self.program[pointer + 1]
+            combo = {4: a, 5: b, 6: c}.get(operand, operand)
+            if op == 0:  # adv
+                a = a // 2**combo
+            elif op == 1:  # bxl
+                b = b ^ operand
+            elif op == 2:  # bst
+                b = combo % 8
+            elif op == 3 and a != 0:  # jnz
+                pointer = operand - 2
+            elif op == 4:  # bxc
+                b = b ^ c
+            elif op == 5:  # out
+                output.append(combo % 8)
+            elif op == 6:  # bdv
+                b = a // 2**combo
+            elif op == 7:  # cdv
+                c = a // 2**combo
+            pointer += 2
+        return ",".join(map(str, output))
 
     def part_one(self) -> str:
-        return self.run_code()
+        return self.run_code(self.registers)
 
     def part_two(self) -> int:
         return 0
