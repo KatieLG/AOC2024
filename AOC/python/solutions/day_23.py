960b26a0e557a536b7c41f97d9a4ebbbd20dd5cd
Katie Gardner <5213160+KatieLG@users.noreply.github.com>
2024-12-23 08:45:40 +0000

    day23 python nicer

--- AOC/python/solutions/day_23.py
+++ AOC/python/solutions/day_23.py
@@ -19,26 +19,24 @@ class Day23(AOCSolution):
 
     @cached_property
     def groups(self) -> dict[str, set[str]]:
-        groups = defaultdict(set)
+        groups: dict[str, set[str]] = defaultdict(set)
         for a, b in self.connections:
             groups[a] |= {b}
             groups[b] |= {a}
         return groups
 
     def part_one(self) -> int:
-        threes = set()
-        for computer in self.groups:
-            group = {computer}
-            for conn in self.groups[computer]:
-                for sub in self.groups[conn]:
-                    if computer in self.groups[sub]:
-                        group |= {conn, sub}
-                        threes.add(tuple(sorted(group)))
-                        group = {computer}
+        threes = {
+            tuple(sorted((computer, conn1, conn2)))
+            for computer in self.groups
+            for conn1 in self.groups[computer]
+            for conn2 in self.groups[conn1]
+            if computer in self.groups[conn2]
+        }
         return sum((any(computer[0] == "t" for computer in three)) for three in threes)
 
     def part_two(self) -> str:
-        connected_groups = set()
+        connected_groups: set[tuple[str, ...]] = set()
         for comp, connections in self.groups.items():
             connected_group = {comp}
             for conn in connections:
